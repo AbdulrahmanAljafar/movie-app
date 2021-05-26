1ecb09ca222f8c1642fc3a01b5f4751b707c8a92
Abdulrahman_Aljafar <abdulrahmana.jafar@gmail.com>
2021-05-26 21:52:06 +0300

    moive app v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
+import One from "./Onecard"
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import json from './movie.json'
+import axios from "axios";
+import { CardGroup, Card, Col,Row } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+  console.log(json)
+  let cards = json.map((item,i)=>{
+    return <One key={i} item={item} />
+  })
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{margin: "auto", padding: "20px"}} className="row d-flex container">
+     {cards}
+
+     </div>
+        
+       
+    
+     
+       
     </div>
   );
 }
